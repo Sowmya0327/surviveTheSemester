6f094500ced8bb8364e97308e5e301641de0702b
SiddharthKarmokar <siddkarmokar@gmail.com>
2026-03-20 19:22:55 +0530

    fix: some dependency problems

--- apps/Frontend/src/pages/Games/campusFighter/src/screens/Home/components/Header.tsx
+++ apps/Frontend/src/pages/Games/campusFighter/src/screens/Home/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Constants } from '../../../../../common/src';
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 import { Space, Text, View } from '../../../components';
 import { titleImage } from '../../../images';
 
